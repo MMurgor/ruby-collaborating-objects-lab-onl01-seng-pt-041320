9be75faedbe9d7db5a6c4677c7c0cc028e49deae
Romena Murgor <romenamurgor@gmail.com>
2020-05-23 17:34:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,12 +4,13 @@ class Song
 
   def initialize(name)
     @name = name
+    @save
   end
   
   def self.all
    @@all
   end
-  save
+  
 
   def self.new_by_filename(file)
     artist_name = file.split(" - ")[0]
@@ -19,6 +20,11 @@ class Song
     song
   end
 
+
+  def save
+    @@all << self
+  end
+  
   def artist_name=(name)
     artist = Artist.find_or_create_by_name(name)
     self.artist = artist
