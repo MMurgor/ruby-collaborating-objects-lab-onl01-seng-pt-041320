9696ea547775f15ce44bcf2902a6fb93f24e6bc4
Romena Murgor <romenamurgor@gmail.com>
2020-05-23 16:58:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,4 @@
-class Song
-  attr_accessor :name, :artist
-
-  def initialize(name)
-    @name = name
-  end
-  
-  def self.all 
-    @@self
-  end
-
-  def self.new_by_filename(file)
-    artist_name = file.split(" - ")[0]
-    song_name = file.split(" - ")[1]
-    song = Song.new(song_name)
-    song.artist_name = artist_name
-    song
-  end
-
-  def artist_name=(name)
+ç
     artist = Artist.find_or_create_by_name(name)
     self.artist = artist
     artist.add_song(self)
