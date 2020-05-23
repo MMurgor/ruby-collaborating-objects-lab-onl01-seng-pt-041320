d321c54fdf461cf79aa017171a0ae9aa2cbe7bbd
Romena Murgor <romenamurgor@gmail.com>
2020-05-23 16:33:06 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,16 +1,13 @@
 class MP3Importer
-
-  attr_accessor :path
+  attr_reader :path, :files
 
   def initialize(path)
     @path = path
-  end
-
-  def files
-    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+    @files = Dir.entries(path).grep(/.*\.mp3/)
   end
 
   def import
-    files.each {|song| Song.new_by_filename(song)}
+    @files.each {|file| Song.new_by_filename(file)}
   end
+end
 
